edf7bb2b2d774b556552627f1de26cbfdb9b546a
aazz85258 <aazz85258@hotmail.com>
2016-11-04 11:51:02 +0800

    update

--- back.php
+++ back.php
@@ -1,4 +1,9 @@
 <?php
-
-echo "hahaha";
+//$flag = $_GET["flag"];
+//if($flag ==1){
+//$list=array("name"=>"wang","sex"=>"man","tel"=>"123","email"=>"xxx@126.com"); 
+//$list1=array("name"=>"sdfa","sex"=>"g","tel"=>"ddd","email"=>"ccc126.com"); 
+//$re = array($list,$list1);
+    //echo '{"records":[{"name":3,"city":"wash"},{"name":5,"city":"22222222"},{"name":6,"city":"a1111111"}]}';
+    echo '{"records":[{"name":3,"city":"wash","cn":"xx"},{"name":3,"city":"wash","cn":"xx"},{"name":3,"city":"wash","cn":"xx"},{"name":3,"city":"wash","cn":"xx"},{"name":3,"city":"wash","cn":"xx"},{"name":3,"city":"wash","cn":"xx"},{"name":5,"city":"22222222","cn":"xx"},{"name":6,"city":"a1111111","cn":"xx"}]}';
 ?>
